667bdcf91dc6c5fdb57d2c464cc0e346b21c6342
Mamadou Oury Sylla <sylm37@uqo.ca>
2025-11-23 19:30:31 -0500

    Correction de l'affichage de la page Accueil via App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Import des pages
 import Accueil from "./pages/Accueil";
 
-import Contenus from "./pages/Contenus";
-
-import DetailContenu from "./pages/DetailContenu";
-
-import Login from "./pages/Login";
-
-import Register from "./pages/Register";
- 
 export default function App() {
-
   return (
-<BrowserRouter>
-<Routes>
-<Route path="/" element={<Accueil />} />
-<Route path="/contenus" element={<Contenus />} />
-<Route path="/contenus/:id" element={<DetailContenu />} />
-<Route path="/login" element={<Login />} />
-<Route path="/register" element={<Register />} />
-</Routes>
-</BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        {/* Page d’accueil */}
+        <Route path="/" element={<Accueil />} />
 
+        {/* Tu ajouteras les autres pages ici plus tard */}
+        {/* <Route path="/disponibilites" element={<Disponibilites />} /> */}
+        {/* <Route path="/mes-reservations" element={<MesReservations />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
-
 }
-
- 
